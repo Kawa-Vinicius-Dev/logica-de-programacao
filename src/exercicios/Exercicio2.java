0c4a21bea53ac0ec57ca1922387ad483c4f2f655
Kawa Vinicius <kawavinicioss@gmail.com>
2026-01-31 14:55:50 -0300

    Atualiza Exercicio3

--- src/exercicios/Exercicio2.java
+++ src/exercicios/Exercicio2.java
@@ -44,5 +44,7 @@ public class Exercicio2 {
 
         System.out.println("Area:" + a);
 
+        input.close();
+
     }
 }
--- src/exercicios/Exercicio3.java
+++ src/exercicios/Exercicio3.java
@@ -1,4 +1,47 @@
 package exercicios;
 
+/*
+Dado o tamanho do lado de um quadrado,
+calcular a área e o perímetro do mesmo
+*/
+
+import java.util.Scanner;
+
 public class Exercicio3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Calculo da área e perimetro de um quadrado: ");
+        System.out.println("Valor do lado: ");
+
+        double lado = ladovalido(input);
+
+        double a = Math.pow(lado, 2);
+        double p = lado * 4;
+
+        System.out.print("Area: " + a + "\nPerimetro: " + p);
+
+        input.close();
+    }
+
+        private static double ladovalido(Scanner input) {
+        double lado;
+        while (true) {
+            if (!input.hasNextDouble()) {
+                System.out.print("Digite uma valor valido: ");
+                input.next();
+                continue;
+            }
+            lado = input.nextDouble();
+            if (lado <= 0) {
+                System.out.print("Digite um valor valido: ");
+                continue;
+            }
+            return lado;
+        }
+
+
+
+    }
 }
