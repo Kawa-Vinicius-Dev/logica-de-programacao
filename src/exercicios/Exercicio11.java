07eb2f6825b5537cd5a163dd8a2be0c638f25bb8
Kawa Vinicius <kawavinicioss@gmail.com>
2026-02-02 17:20:33 -0300

    adiciona Exercicio11 - converte número inteiro em binário

---
+++ src/exercicios/Exercicio11.java
@@ -0,0 +1,43 @@
+package exercicios;
+
+/*
+ * Converter um inteiro informado menor que 32
+ * para sua representação em binário
+ */
+
+import java.util.Scanner;
+
+public class Exercicio11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num = lerNumeroValido(input, "o numero");
+        converterNumeroBinario(num);
+
+        input.close();
+    }
+
+    private static int lerNumeroValido(Scanner input, String nomeCampo) {
+        while (true) {
+            System.out.println("Digite " + nomeCampo + ": ");
+
+            if (!input.hasNextInt()) {
+                System.out.println("Digite um número valido! ");
+                input.next();
+                continue;
+            }
+            int numero = input.nextInt();
+
+            if (numero == 0 || numero >= 32) {
+                System.out.println("Digite um número de 0 a 31!");
+                continue;
+            }
+            return numero;
+        }
+    }
+
+    private static void converterNumeroBinario(int num) {
+        String binario = Integer.toBinaryString(num);
+        System.out.println(num + " é igual a: " + binario);
+    }
+}
