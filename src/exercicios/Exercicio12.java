4b59cbcd3dfca1fa568a4a4a44a409cb8f0fda9b
Kawa Vinicius <kawavinicioss@gmail.com>
2026-02-03 12:52:10 -0300

    adiciona Exercicio12 - calcula nota semestral

--- src/exercicios/Exercicio12.java
+++ src/exercicios/Exercicio12.java
@@ -46,7 +46,7 @@ public class Exercicio12 {
         return nota;
     }
 
-    private static double calculoNotaSemestral(double nota1, double nota2) {
+    private static double calculoNotaSemestral(double nota1, double nota2){
         double notaSemestral = (nota1 + nota2) / 2;
         System.out.println("Nota Semestral " + notaSemestral);
         return notaSemestral;
