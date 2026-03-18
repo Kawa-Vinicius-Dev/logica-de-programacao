1b292204fd8b50c6a3632e7cce77f51c12ac66b5
Kawa Vinicius <kawavinicioss@gmail.com>
2026-03-17 21:00:51 -0300

    feat: adiciona Exercicio30 - calcula o desconto previdenciário de um funcionário

---
+++ src/exercicios/Exercicio30.java
@@ -0,0 +1,60 @@
+package exercicios;
+
+/*
+* Escreva um programa que calcula o desconto previdenciário de um funcionário. Dado um
+* salário, o programa deve retornar o valor do desconto proporcional ao mesmo. O cálculo
+* segue a regra: o desconto é de 11% do valor do salário, entretanto, o valor máximo de
+* desconto é 334,29, o que seja menor
+*/
+
+import java.util.Scanner;
+
+public class Exercicio30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double salario = verificarNumero(sc);
+        double previdencia = calcularPrevidencia(salario);
+
+    }
+
+    private static double verificarNumero(Scanner sc) {
+        boolean verificar = true;
+        double numero = 0;
+        do {
+            System.out.print("Digite o salário: ");
+            if (!sc.hasNextDouble()) {
+                System.out.println("Digite um número valido!");
+                sc.next();
+                verificar = false;
+            }
+
+            numero = sc.nextDouble();
+            verificar = true;
+
+            if (numero <= 0) {
+                System.out.println("Digite um número maior que zero!");
+                sc.next();
+                verificar = false;
+            }
+        }while (!verificar);
+        return numero;
+    }
+    private static double calcularPrevidencia(double salario) {
+
+        double previdencia =  salario * 0.11;
+        double max = 334.29;
+
+        if (previdencia > max){
+            System.out.println("Previdência do funcionário é de: " + previdencia +
+                    " \nValor maximo da previdencia é de: " + max);
+        }else if (previdencia == max) {
+            System.out.println("Previdência do funcionário é de: " + previdencia +
+                    " \nEsta igual ao valor máximo: " + max);
+        }else{
+            System.out.println("Previdência do funcionário é de: " + previdencia +
+                    " \nValor maximo da previdencia é de: " + max);
+        }
+            return previdencia;
+    }
+}
