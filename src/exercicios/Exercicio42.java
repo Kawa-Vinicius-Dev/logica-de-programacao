3a3e9ec4d6a2d153477dd60fb35af7e9d09a57b0
Kawa Vinicius <kawavinicioss@gmail.com>
2026-07-28 15:58:42 -0300

    feat: adiciona contagem de 1 à 100. for

---
+++ src/exercicios/Exercicio42.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+/*
+ Escreva um programa que imprima todos os números inteiros do intervalo fechado de 1 a
+100.
+ */
+public class Exercicio42 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
