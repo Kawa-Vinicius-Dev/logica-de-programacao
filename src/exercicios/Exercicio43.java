a3f786f811e61356460424e5b2510922a6dc9cdd
Kawa Vinicius <kawavinicioss@gmail.com>
2026-07-28 17:32:21 -0300

    feat: importa inteiros de 100 a 1. for

---
+++ src/exercicios/Exercicio43.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+/*
+Escreva um programa que imprima todos os números inteiros de 100 a 1 (em ordem
+decrescente)
+*/
+public class Exercicio43 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i--) {
+            System.out.printf("%d ",i);
+        }
+    }
+}
